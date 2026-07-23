CeladonSS13/Shiptest

Eye wounds + new quirk (#5897)
Being shot or stabbed in the eyes without wearing eye protection now has
a small chance to cause an eye injury, impairing vision. Scarring on
both eyes causes total blindness.

<img width="148" height="146" alt="akesi_moment"
src="https://github.com/user-attachments/assets/8dd3f32f-8905-49aa-bc81-878ac7eee422"
/>

<img width="460" height="460" alt="image"
src="https://github.com/user-attachments/assets/69f6c47f-91b4-440a-9212-81fb7a7b7eb6"
/>

This also adds a new quirk that applies a scar on one eye or both.

Largely ported from tgstation/tgstation#87209 by SmArtKar

From the original PR:
> Its a neat lil' feature that makes the game more immersive, and
unlocks more roleplay opportunities for players. New quirk gives access
to this feature for players who want to make it a part of their
character's backstory (or maybe as a part of permanent scar
roleplaying).

:cl: smartkar, sapphicoverload
add: Added eye injuries, remember your safety goggles!
fix: Fixed eye damage overlay not being cleared when removing/replacing
damaged eyes.
/:cl:

code/modules/mob/living/carbon/human/species.dm
@@ -190,6 +190,9 @@ GLOBAL_LIST_EMPTY(roundstart_races)
 	var/sound/attack_sound = 'sound/weapons/punch1.ogg'
 	var/sound/miss_sound = 'sound/weapons/punchmiss.ogg'
 
+	///Cache of generated eye icons.
+	var/static/list/masked_eye_icons_cache = list()
+
 	///What gas does this species breathe? Used by suffocation screen alerts, most of actual gas breathing is handled by mutantlungs. See [life.dm][code/modules/mob/living/carbon/human/life.dm]
 	var/breathid = "o2"
 
@@ -713,16 +716,37 @@ GLOBAL_LIST_EMPTY(roundstart_races)
 			if(eyes)
 				if(!HAS_TRAIT(H, TRAIT_EYESCLOSED) && !(H.stat == DEAD))
 
-					if(iskepori(H)) // Kepori need sclera but don't fit the normal silhouette, so this needs changing. Make better later.
-						eye_overlay = mutable_appearance('icons/mob/species/kepori/kepori_eyes.dmi', eyes.eye_icon_state, -BODYPARTS_LAYER)
-						sclera_overlay = mutable_appearance('icons/mob/species/kepori/kepori_eyes.dmi', eyes.sclera_icon_state, -BODYPARTS_LAYER)
-					// [CELADON-ADD] - CELADON_LANIUS
-					else if(islanius(H))
-						eye_overlay = mutable_appearance('mod_celadon/_storage_icons/icons/species/lanius/lanius_organs.dmi', eyes.eye_icon_state, -BODYPARTS_LAYER)
-					// [/CELADON-ADD]
+					var/icon/eye_icon
+					var/icon/sclera_icon
+					var/icon_cache_key = "[eyes.eye_icon_state]-[eyes.sclera_icon_state]-[id]-[eyes.scarring]"
+					if(!masked_eye_icons_cache[icon_cache_key])
+						if(iskepori(H)) // Kepori need sclera but don't fit the normal silhouette, so this needs changing. Make better later.
+							eye_icon = icon('icons/mob/species/kepori/kepori_eyes.dmi', eyes.eye_icon_state)
+							sclera_icon = icon('icons/mob/species/kepori/kepori_eyes.dmi', eyes.sclera_icon_state)
+						// [CELADON-ADD] - CELADON_LANIUS
+						else if(islanius(H))
+							eye_overlay = mutable_appearance('mod_celadon/_storage_icons/icons/species/lanius/lanius_organs.dmi', eyes.eye_icon_state, -BODYPARTS_LAYER)
+						// [/CELADON-ADD]
+						else
+							eye_icon = icon(species_eye_path || 'icons/mob/human_face.dmi', eyes.eye_icon_state)
+							sclera_icon = icon('icons/mob/human_face.dmi', eyes.sclera_icon_state)
+
+						if(eyes.scarring & RIGHT_EYE_SCAR)
+							var/icon/right_scar_mask = icon('icons/mob/eye_masks.dmi', "right_eye")
+							eye_icon.Blend(right_scar_mask, ICON_MULTIPLY)
+							sclera_icon.Blend(right_scar_mask, ICON_MULTIPLY)
+						if(eyes.scarring & LEFT_EYE_SCAR)
+							var/icon/left_scar_mask = icon('icons/mob/eye_masks.dmi', "left_eye")
+							eye_icon.Blend(left_scar_mask, ICON_MULTIPLY)
+							sclera_icon.Blend(left_scar_mask, ICON_MULTIPLY)
+
+						masked_eye_icons_cache[icon_cache_key] = list(eye_icon, sclera_icon)
 					else
-						eye_overlay = mutable_appearance(species_eye_path || 'icons/mob/human_face.dmi', eyes.eye_icon_state, -BODYPARTS_LAYER)
-						sclera_overlay = mutable_appearance('icons/mob/human_face.dmi', eyes.sclera_icon_state, -BODYPARTS_LAYER)
+						eye_icon = masked_eye_icons_cache[icon_cache_key][1]
+						sclera_icon = masked_eye_icons_cache[icon_cache_key][2]
+
+					eye_overlay = mutable_appearance(eye_icon, layer = -BODY_LAYER)
+					sclera_overlay = mutable_appearance(sclera_icon, layer = -BODY_LAYER)
 
 					if(HD.greyscale_eyes && eyes)
 						eye_overlay.color = "#" + H.eye_color

SOLARIS-Collective/Solaris

code/modules/mob/living/carbon/human/species.dm
@@ -190,6 +190,9 @@ GLOBAL_LIST_EMPTY(roundstart_races)
 	var/sound/attack_sound = 'sound/weapons/punch1.ogg'
 	var/sound/miss_sound = 'sound/weapons/punchmiss.ogg'
 
+	///Cache of generated eye icons.
+	var/static/list/masked_eye_icons_cache = list()
+
 	///What gas does this species breathe? Used by suffocation screen alerts, most of actual gas breathing is handled by mutantlungs. See [life.dm][code/modules/mob/living/carbon/human/life.dm]
 	var/breathid = "o2"
 
@@ -713,16 +716,37 @@ GLOBAL_LIST_EMPTY(roundstart_races)
 			if(eyes)
 				if(!HAS_TRAIT(H, TRAIT_EYESCLOSED) && !(H.stat == DEAD))
 
-					if(iskepori(H)) // Kepori need sclera but don't fit the normal silhouette, so this needs changing. Make better later.
-						eye_overlay = mutable_appearance('icons/mob/species/kepori/kepori_eyes.dmi', eyes.eye_icon_state, -BODYPARTS_LAYER)
-						sclera_overlay = mutable_appearance('icons/mob/species/kepori/kepori_eyes.dmi', eyes.sclera_icon_state, -BODYPARTS_LAYER)
-					// [MANKIND-ADD] - MANKIND_LANIUS
-					else if(islanius(H))
-						eye_overlay = mutable_appearance('modular_mankind/_storage_icons/icons/species/lanius/lanius_organs.dmi', eyes.eye_icon_state, -BODYPARTS_LAYER)
-					// [/MANKIND-ADD]
+					var/icon/eye_icon
+					var/icon/sclera_icon
+					var/icon_cache_key = "[eyes.eye_icon_state]-[eyes.sclera_icon_state]-[id]-[eyes.scarring]"
+					if(!masked_eye_icons_cache[icon_cache_key])
+						if(iskepori(H)) // Kepori need sclera but don't fit the normal silhouette, so this needs changing. Make better later.
+							eye_icon = icon('icons/mob/species/kepori/kepori_eyes.dmi', eyes.eye_icon_state)
+							sclera_icon = icon('icons/mob/species/kepori/kepori_eyes.dmi', eyes.sclera_icon_state)
+						// [CELADON-ADD] - CELADON_LANIUS
+						else if(islanius(H))
+							eye_overlay = mutable_appearance('modular_mankind/_storage_icons/icons/species/lanius/lanius_organs.dmi', eyes.eye_icon_state, -BODYPARTS_LAYER)
+						// [/CELADON-ADD]
+						else
+							eye_icon = icon(species_eye_path || 'icons/mob/human_face.dmi', eyes.eye_icon_state)
+							sclera_icon = icon('icons/mob/human_face.dmi', eyes.sclera_icon_state)
+
+						if(eyes.scarring & RIGHT_EYE_SCAR)
+							var/icon/right_scar_mask = icon('icons/mob/eye_masks.dmi', "right_eye")
+							eye_icon.Blend(right_scar_mask, ICON_MULTIPLY)
+							sclera_icon.Blend(right_scar_mask, ICON_MULTIPLY)
+						if(eyes.scarring & LEFT_EYE_SCAR)
+							var/icon/left_scar_mask = icon('icons/mob/eye_masks.dmi', "left_eye")
+							eye_icon.Blend(left_scar_mask, ICON_MULTIPLY)
+							sclera_icon.Blend(left_scar_mask, ICON_MULTIPLY)
+
+						masked_eye_icons_cache[icon_cache_key] = list(eye_icon, sclera_icon)
 					else
-						eye_overlay = mutable_appearance(species_eye_path || 'icons/mob/human_face.dmi', eyes.eye_icon_state, -BODYPARTS_LAYER)
-						sclera_overlay = mutable_appearance('icons/mob/human_face.dmi', eyes.sclera_icon_state, -BODYPARTS_LAYER)
+						eye_icon = masked_eye_icons_cache[icon_cache_key][1]
+						sclera_icon = masked_eye_icons_cache[icon_cache_key][2]
+
+					eye_overlay = mutable_appearance(eye_icon, layer = -BODY_LAYER)
+					sclera_overlay = mutable_appearance(sclera_icon, layer = -BODY_LAYER)
 
 					if(HD.greyscale_eyes && eyes)
 						eye_overlay.color = "#" + H.eye_color