CeladonSS13/Shiptest

FIX: Robot Wounds

code/modules/mob/living/carbon/human/species_types/spider.dm
@@ -8,7 +8,7 @@
 	name = "Rachnid"
 	id = SPECIES_RACHNID
 	default_color = "00FF00"
-	species_traits = list(LIPS, MUTCOLORS_PARTSONLY, HAS_FLESH, HAS_BONE)	// [CELADON-EDIT]
+	species_traits = list(LIPS, MUTCOLORS_PARTSONLY)	// [CELADON-EDIT]
 	inherent_biotypes = MOB_ORGANIC|MOB_HUMANOID|MOB_BUG
 	mutant_bodyparts = list("spider_legs", "spider_spinneret")
 	default_features = list("spider_legs" = "Carapaced", "spider_spinneret" = "Plain")

SOLARIS-Collective/Solaris

code/modules/mob/living/carbon/human/species_types/spider.dm
@@ -8,7 +8,7 @@
 	name = "Rachnid"
 	id = SPECIES_RACHNID
 	default_color = "00FF00"
-	species_traits = list(LIPS, MUTCOLORS_PARTSONLY, HAS_FLESH, HAS_BONE)	// [MANKIND-EDIT]
+	species_traits = list(LIPS, MUTCOLORS_PARTSONLY)	// [MANKIND-EDIT]
 	inherent_biotypes = MOB_ORGANIC|MOB_HUMANOID|MOB_BUG
 	mutant_bodyparts = list("spider_legs", "spider_spinneret")
 	default_features = list("spider_legs" = "Carapaced", "spider_spinneret" = "Plain")